c6e203887ffd0ab3c3da2f2c0c4d59e7110fce71
Christian Bird <cabird@gmail.com>
2010-01-18 19:05:53 -0800

    Added continuous.rb
    This monitors a paper directory and executes
    rake whenever a .tex or .bib file is changes.
    The logic is a bit too simple right now, but it
    works.

---
+++ continuous/continuous.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+
+# This class tracks which files in a directory have changed
+# pass the path to the directory to the constructor.  Then,
+# each time whatChanged? is called, it will list the files
+# that have been modified (or added, but not deleted) since
+# the previous call
+class DirMonitor
+	attr_reader :dir
+	def initialize(dir)
+		@dir = dir
+		@mTimes = {}
+		# prime @mTimes with the known modification times
+		Dir.foreach(@dir) { |file| path = File.join(@dir, file)
+			@mTimes[path] = File.mtime(path)
+		}
+	end
+
+	# which files have changed since we last checked
+	def whatChanged?
+		changed = []
+		Dir.foreach(@dir) do |file|
+			path = File.join(@dir, file)
+			#file can actually disappear midstream...
+			if File.exists? path
+				mtime = File.mtime(path)
+				changed << path if !@mTimes.key?(path) || @mTimes[path] < File.mtime(path)
+				#update with the latest mod time
+				@mTimes[path] = mtime
+			end
+		end
+		return changed
+	end
+end
+
+# notifier can be any program that outputs something
+# to std out when any of the directories on the command
+# line have changed.  Note that programs may buffer to
+# std out if it is not to a tty.  Thus, make sure you
+# fflush stdout or you may think there is a bug in this code
+
+# BSDs (including OS X) support kqueue, so use kq in that case
+$NOTIFIER = File.join(File.dirname(__FILE__), "kq_notifier")
+
+# Linux uses inotify so use that if needed
+$NOTIFIER = File.join(File.dirname(__FILE__), "inotify_notifier")
+
+def main
+	dirs = ["."]
+	dirMonitor = DirMonitor.new(dirs[0])
+
+	notifyOut = IO.popen("#{$NOTIFIER} #{dirs.join ' '}")
+	puts "#{$NOTIFIER} is now running"
+	while true
+		fds = IO.select([notifyOut], [], [])
+		if fds
+			fds[0].each { |fd|
+				s = fd.readline.strip
+				#probably should do something here
+			}
+			changed = dirMonitor.whatChanged?
+			puts "changed " + changed.inspect if !changed.empty?
+			if changed.any? { |x| x =~ /\.(bib|tex)$/ }
+				puts "Detected changes in: #{changed.join ' '}"
+				puts "rebuilding now"
+				system("rake")
+			end
+		end
+	end
+end
+
+main if __FILE__ == $0
+
+
